43d2c17f6809a804f221df1dcb18578ddfcd38a5
mahfoozalamcse <amahfooz1007@gmail.com>
2025-02-09 13:39:29 +0530

    complete basic js part 1

---
+++ part1/operation.js
@@ -0,0 +1,85 @@
+let add = 4 + 5; 
+console.log(add);
+
+let sum = 90;
+
+let addition = add + sum;
+console.log(addition);
+
+
+// subtract 
+
+let subtract = sum - add;
+console.log(subtract);
+
+// multiplication
+let multipy = sum*add;
+console.log(multipy);
+
+// remainder
+let remainder = 70 % 29;
+console.log(remainder);
+
+// divide 
+let devide = 23 / 4;
+console.log(devide);
+
+// power
+let pow = 2 ** 3;
+console.log(pow);
+
+// increament and decreament
+let inc = 10;
+inc++;
+console.log(inc);
+
+++inc;
+console.log(inc);
+
+// decreament 
+let dec = 10;
+dec--;
+console.log(dec);
+--dec;
+console.log(dec);
+
+// comprision
+let num2 = 3;
+let num3 = 3;
+let num4 = 6;
+
+console.log(num2 == num3); // true
+console.log(num2 != num4); // true
+
+console.log(num2 > num3);
+console.log(num4 > num3);
+
+
+// logical operator
+// && , || , !
+console.log(0 && 1);
+console.log(0 || 1);
+console.log(0 != 1);
+
+// assighnment operator
+let sum5 = 29 + 11;
+let sum6 = 11;
+console.log(sum6);
+
+// operator preceedance
+let oppreceed = 12 * 34 / 12 % 23 +(12 + 23) * 12;
+console.log(oppreceed);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
