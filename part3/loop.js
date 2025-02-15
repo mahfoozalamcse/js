4f87eb44f92493b3859d88ac716843865c209496
mahfoozalamcse <amahfooz1007@gmail.com>
2025-02-15 23:36:46 +0530

    use promt add element in array

--- part3/loop.js
+++ part3/loop.js
@@ -7,3 +7,25 @@ while (i <= 5) {
     i++;
 }
 console.log(sum);
+
+// write array countDown and push element in array 
+let countDown = [];
+let j = 5;
+while (j>0) {
+    countDown.push(j);
+    j--;
+}
+console.log(countDown);
+
+// let teacollection and use promt to add
+let teacollection = [];
+let tea;
+do {
+   tea = prompt(`Enter your favourite tea (type "stop" to finish)`);
+   if (tea !== "stop") {
+     teacollection.push(tea);
+   } 
+} while (tea !== "stop");
+
+// here not run in node js use browswer and test
+
