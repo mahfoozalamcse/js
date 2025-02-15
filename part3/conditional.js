8a92e58ce4df58ffaddf90a5f94c127babdb5ffa
mahfoozalamcse <amahfooz1007@gmail.com>
2025-02-15 13:53:13 +0530

    learn and practice conditional

---
+++ part3/conditional.js
@@ -0,0 +1,50 @@
+// conditional challanges
+
+// 1. check number greater
+let num1 = 12;
+let num2 = 13;
+if (num1 > num2) {
+    console.log(num1);
+    
+}else{
+    console.log(num2);
+    
+}
+
+// check string is equal to another string
+
+let userName = "Mahfooz";
+let userName2 = "Alam";
+
+if (userName === userName2) {
+    console.log("equal");
+    
+}else{
+    console.log("Nope, Not equal");
+    
+}
+
+// check if boolean value true or false
+
+let isReady = false;
+if (isReady) {
+    console.log("Yep, is ready");
+    
+}else{
+    console.log("Nope, is not ready");
+    
+}
+
+// check if array is empty or not
+let arr = ["Alam"];
+console.log(arr.length);
+
+if (arr.length === 0) {
+    console.log("Array is empty");
+}else{
+    console.log("Array not empty");
+    
+}
+
+
+
