b78bbe4c60ef3aafbe9c2c422f48a582a5912a3a
mahfoozalamcse <amahfooz1007@gmail.com>
2025-02-19 15:44:08 +0530

    practice higher order function and practice problem

---
+++ part4/function2.js
@@ -0,0 +1,26 @@
+// take function inside function
+// passed function as variable
+function makeTea(typeofTea){
+    return `make tea ${typeofTea}`
+};
+function orderTea(teaFunction){
+    return teaFunction('earl grey');
+}
+let proceesOrder = orderTea(makeTea);
+//console.log(proceesOrder);
+
+
+// function pass inside function is example of higher order eg. foreach loop use 
+
+// practice
+function createTeaMaker(name){
+    let score = 100;
+    return function(teaType){
+        return `Making ${teaType} and strange js ${name} is ${score}`
+    };
+}
+let teaMake = createTeaMaker("mahfooz");
+let result = teaMake("Lamon tea");
+console.log(result);
+
+
