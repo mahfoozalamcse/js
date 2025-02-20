475b26f5b0d1a1615843cddbdb9aae91d4ca5ba3
mahfoozalamcse <amahfooz1007@gmail.com>
2025-02-20 20:57:07 +0530

    understand asyncronous and syncronous

---
+++ part8/advance.js
@@ -0,0 +1,20 @@
+//syncronous
+console.log("Hello js");
+
+for (let index = 0; index < 9; index++) {
+    console.log(index);
+     
+}
+
+// asysncronous (also say like paus and execute)
+console.log("Hello");
+function sayHello(){
+    console.log("I would like to say hello ");
+}
+setTimeout(() => {
+    sayHello()
+}, 4000);
+console.log("mahfooz");
+
+// that above asyn behaviour of js
+
