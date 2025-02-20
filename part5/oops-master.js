7b1fa143a1ca45fc665234ee99cf2201837eec11
mahfoozalamcse <amahfooz1007@gmail.com>
2025-02-20 12:58:30 +0530

    unsersatand inheritance and constructor and all key concept

--- part5/oops-master.js
+++ part5/oops-master.js
@@ -14,9 +14,51 @@ function Person(name, age){
     this.age = age;
 }
 let john = new Person("john", 22);
-console.log(john);
-console.log(john.age);
+// console.log(john);
+// console.log(john.age);
 
 // prototype and prototypal  chain
+function Animal(type){
+    this.type = type;
+}
+Animal.prototype.speak = function(){
+    return `${this.type} make a sound`
+}
+let dog = new Animal("Dog");
+// console.log(dog);
 
+// 
+Array.prototype.mahfooz = function(){
+    return `Custom method ${this}`
+}
+let myArr = [1, 2, 3, 4];
+console.log(myArr.mahfooz());
+let myNewArr = [1, 2, 3];
+console.log(myNewArr.mahfooz());
 
+// class
+class Vehicle {
+    constructor(make, model) {
+        this.make = make;
+        this.model = model;
+    }
+    start(){
+        return `${this.model} is a car from ${this.make}`
+    }
+}
+
+// inheritance borrow property from vehicle class
+
+class Car extends Vehicle {
+    drive() {
+        return `${this.make} this is inheritace example`
+    }
+}
+
+// object
+let myCar = new Car("Toyota" , "Carolla")
+console.log(myCar.start());
+console.log(myCar.drive());
+
+ // let vehOne = Vehicle("Toyota", "Camary") //error  Class constructor Vehicle cannot be invoked without 'new'
+// console.log(vehOne.make);
