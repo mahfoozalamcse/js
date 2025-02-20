0d88b047c9d40d9d8dd424dea1dd1b1dddde523d
mahfoozalamcse <amahfooz1007@gmail.com>
2025-02-20 14:16:52 +0530

    completed oops core with practice

--- part5/oops-master.js
+++ part5/oops-master.js
@@ -57,8 +57,109 @@ class Car extends Vehicle {
 
 // object
 let myCar = new Car("Toyota" , "Carolla")
-console.log(myCar.start());
-console.log(myCar.drive());
+// console.log(myCar.start());
+// console.log(myCar.drive());
 
  // let vehOne = Vehicle("Toyota", "Camary") //error  Class constructor Vehicle cannot be invoked without 'new'
 // console.log(vehOne.make);
+
+// Encapsulation
+
+class Bankaccount{
+    #balance = 0;
+    deposite(amaount){
+        this.#balance += amaount;
+        return this.#balance;
+    }
+    getBalance(){
+        return `$ ${this.#balance}`
+    }
+}
+let account = new Bankaccount();
+
+// console.log(account.balance);
+
+// console.log(account.#balance); // not excess outside
+// console.log(account.getBalance()); // you can acces
+
+// explore more 
+
+// Abstraction hide complex detail
+class coffieMachine{
+    start(){
+         // call DB
+        // filter value
+     return `Startig the machine .. `
+    }
+    berwCoffie(){
+      // complex calculation
+      return `Brewing....`
+    }
+    pressButtonStart(){
+        let mass1 = this.start();
+        let mass2 = this.berwCoffie();
+        return `${mass1}  ${mass2}`
+    }
+}
+
+let myMachine = new coffieMachine();
+// console.log(myMachine.start());
+// console.log(myMachine.berwCoffie());
+// console.log(myMachine.pressButtonStart());
+
+// polymorphism
+class Bird{
+    flying(){
+        return `flying...`
+    }
+}
+class Penguin extends Bird{
+    flying(){
+        return `Can't fly...`
+    }
+}
+
+let bird = new Bird();
+let penguin = new Penguin();
+// console.log(bird.flying());
+// console.log(penguin.flying());
+
+// static method
+class Calculator{
+    static add(a, b){
+        return a+b;
+    }
+}
+// let minCalc = new Calculator();
+// console.log(minCalc.add(2,3)); // give error minCalc.add is not a function
+// you call without creating object 
+
+// console.log(Calculator.add(2, 7));
+
+// getter and setter // all about more controll 
+class Employee {
+    #salary;
+    constructor(name, salary) {
+        if (salary < 0) {
+            throw new Error("Salary not be negative");
+            
+        }
+       this.name = name;
+       this.#salary = salary; 
+    }
+    get salary(){
+        return `You are not allowed to see salary`;
+    }
+    set salary(value){
+        if (value < 0) {
+            console.error("Invalid Salary")
+        }else{
+            this._salary = value;
+        }
+    }
+}
+let employee = new Employee("Mahfooz", 50000);
+console.log(employee._salary);
+employee.salary = -60000;
+
+
