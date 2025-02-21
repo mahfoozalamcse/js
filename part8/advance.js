22be0f087b0308e69f5ca53a0a105d4f310d61e9
mahfoozalamcse <amahfooz1007@gmail.com>
2025-02-21 21:53:53 +0530

    practice

--- part8/advance.js
+++ part8/advance.js
@@ -9,11 +9,11 @@ for (let index = 0; index < 9; index++) {
 // asysncronous (also say like paus and execute) not follow sequence
 console.log("Hello");
 function sayHello(){
-    
+
     console.log("I would like to say hello ");
 }
 setTimeout(() => {
-    sayHello()
+    sayHello();
 }, 4000);
 console.log("mahfooz");
 
