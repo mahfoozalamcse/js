83a3d444d49f544312149f8f4e06402c3d3912a4
mahfoozalamcse <amahfooz1007@gmail.com>
2025-02-25 21:29:25 +0530

    import module in appjs

---
+++ part8/appModule.js
@@ -0,0 +1,14 @@
+// Module import file
+
+// defualt import
+
+import  multiply  from "./operationMathModule.js";
+
+// named import
+import { add, substaract } from "./operationMathModule.js";
+
+console.log(multiply(2, 2));
+console.log(add(2, 2));
+console.log(substaract(3, 9));
+
+
