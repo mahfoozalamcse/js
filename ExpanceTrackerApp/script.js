8eac0a7fbd0d4a9a93ab5aea893993cc44e57c38
mahfoozalamcse <amahfooz1007@gmail.com>
2025-02-26 19:37:35 +0530

    js file start working forword next time

---
+++ ExpanceTrackerApp/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () =>{
+
+    const expanseForm = document.getElementById("expence-form");
+    const expanseNameInput = document.getElementById("expence-name");
+    const expanseAmountInput = document.getElementById("expence-amount");
+    const expanseList = document.getElementById("expence-list");
+    const totalAmountDisplay = document.getElementById("total-amount");
+
+    let expenses = [];
+    let totalAmount  = calculateTotal();
+
+    function calculateTotal(params) {
+        
+    }
+
+})
