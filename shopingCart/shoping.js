c339153152df0d7d561280e02e1e89bce5f26af3
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-03-15 19:47:55 +0530

    Update shoping.js

--- shopingCart/shoping.js
+++ shopingCart/shoping.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
  const products = [
     {id : 1, name : "Product 1", price : 28.00},
     {id : 2, name : "Product 2", price : 18.70},
-    {id : 3, name : "Product 3", price : 48.90},
+    {id : 3, name : "Product 3", price : 48.90}, 
     {id : 4, name : "Product 4", price : 58.90},
  ];
    
