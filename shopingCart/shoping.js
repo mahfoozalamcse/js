8f9936a2efcd71caef742e687aefd903871eb05f
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-03-18 21:24:14 +0530

    Update shoping.js

--- shopingCart/shoping.js
+++ shopingCart/shoping.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
  products.forEach(product =>{
     const productDiv = document.createElement('div')
     productDiv.classList.add('product')
-
+ 
     productDiv.innerHTML = `
     <span> ${product.name} - $${product.price.toFixed(2)} </span>
     <button data-id="${product.id}">Add to cart </button>`
