2ec73f423631035cc5875507d0af462455d5b626
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-03-18 21:26:19 +0530

    Update todo.js

--- todo/todo.js
+++ todo/todo.js
@@ -10,7 +10,7 @@ addTaskButton.addEventListener('click' , () => {
      const taskText = todoInput.value.trim(); 
      if (taskText === "") return;
      const newTask = { 
-        id : Date.now(),
+        id : Date.now(), 
         text : taskText,
         completed : false,
      }
