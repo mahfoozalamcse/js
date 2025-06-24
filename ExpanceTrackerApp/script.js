130401c92bff0f71be99173bdee62a3bff19dbdb
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-06-24 23:40:00 +0530

    Update script.js

--- ExpanceTrackerApp/script.js
+++ ExpanceTrackerApp/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () =>{ 
  
     const expanseForm = document.getElementById("expence-form");  
-    const expanseNameInput = document.getElementById("expence-name");  
+    const expanseNameInput = document.getElementById("expence-name");
     const expanseAmountInput = document.getElementById("expence-amount");  
     const expanseList = document.getElementById("expence-list");
     const totalAmountDisplay = document.getElementById("total-amount");
