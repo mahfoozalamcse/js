b49ea5dc81aec1d5de50c5c89cee38e613cb1591
mahfoozalamcse <amahfooz1007@gmail.com>
2025-06-26 17:23:49 +0530

    basic js data type

---
+++ part9/geek1.js
@@ -0,0 +1,74 @@
+// string data type
+let userName = "Mahfooz";
+console.log(userName);
+console.log(typeof userName);
+
+// let username = mahfooz;
+// console.log(username);   // give error not defined
+// console.log(typeof username); // type undefined
+
+
+
+// Number data type
+let age = 26;
+console.log(age);
+console.log(typeof age);
+
+// Boolean data type 
+let isProduct = true;
+console.log(isProduct);
+
+if (isProduct) {
+    console.log("product is avilable");
+}else{
+    console.log("not avilable");
+    
+}
+
+console.log(typeof isProduct);
+
+
+let isProductInCart = "true";  // treated as a string
+console.log(typeof isProductInCart);  // Output: string
+
+
+// udedefined data type
+
+let user;
+console.log(user);
+console.log(typeof user);
+
+// Null data type
+let user1 = null;
+console.log(user1); // Outputs: null
+console.log(typeof user1); // Outputs: object
+
+// object data type
+const person = {
+    user : "Mahfooz",
+    age  : 23,
+    maritalStatus : "Single",
+    education : "Engineer"
+
+}
+
+console.log(person);
+console.log(typeof person);
+
+// Array data type
+let number  = [1, 2, 3, 4, 5];
+console.log(number);
+console.log(typeof number);
+
+
+
+
+
+
+
+
+
+
+
+
+
