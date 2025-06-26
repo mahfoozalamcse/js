884cf96a79941f7c2a12c85a1e3aaaef44f9c25e
mahfoozalamcse <amahfooz1007@gmail.com>
2025-06-26 22:12:08 +0530

    add readline sync module

---
+++ part10/7.arithmetic.js
@@ -0,0 +1,16 @@
+const x = "mango";
+const y = "apple";
+
+console.log(x + y);
+
+console.log(x - y);
+
+console.log(x * y);
+
+console.log(x / y);
+
+console.log(x % y);
+
+console.log(x ** 2);
+
+console.log(y ** 3);
