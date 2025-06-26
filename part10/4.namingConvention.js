ebcd2d13e08ddc794b4093f2cc037f572c6f8841
mahfoozalamcse <amahfooz1007@gmail.com>
2025-06-26 21:44:55 +0530

    var let and const and naming convention

---
+++ part10/4.namingConvention.js
@@ -0,0 +1,19 @@
+let userName = "Mahfooz";
+let userAge;
+let userHomeAddress;
+
+let brotherAge;
+
+let Age;
+
+// let _;
+
+let $ = "Hello";
+console.log($)
+
+// let number1;
+
+// let number_2;
+
+
+console.log(userName);
