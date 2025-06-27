c52f2bf68edb6271473e992fecb5ddd69cd32b27
mahfoozalamcse <amahfooz1007@gmail.com>
2025-06-27 10:49:48 +0530

    change name folder

---
+++ part10-M1/2.variable.js
@@ -0,0 +1,21 @@
+ var message;
+
+// message = "mahfooz";
+
+// message = "My name is mahfooz";
+
+// console.log(message);
+
+let text; //Creating Variable or Declaring a variable
+
+text = "JavaScript is the best!!!"; // Assigning a value to a variable
+
+// console.log(text);
+
+// let myname = "Mahfooz Alam!!"
+
+// console.log(myname);
+
+const number = 1;
+
+console.log(number);
---
+++ part10-M1/3.var_let_const.js
@@ -0,0 +1,11 @@
+// var message = "Hello";
+// var message = "Hii";
+// var message = "Bye";
+// console.log(message);
+
+// let message1 = "Hello";
+// message1 = "Hiii";
+// console.log(message1);
+
+const message2 = "hello";
+console.log(message2);
