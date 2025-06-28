c22511da066ba3bf4b7a5fda446eb61a4f871211
mahfoozalamcse <amahfooz1007@gmail.com>
2025-06-28 11:14:53 +0530

    added asyn await module and also practice

---
+++ asynJs/create-promis.js
@@ -0,0 +1,25 @@
+/**
+ * Promise
+ */
+
+const isRequestSuccessful = false;
+
+const db = [
+  { id: 1, name: "mahfooz" },
+  { id: 2, name: "prakash" },
+];
+
+let promise = new Promise((resolve, reject) => {
+  if (isRequestSuccessful) {
+    setTimeout(() => resolve(db), 3000);
+  } else {
+    const error = new Error("Something went wrong");
+    reject(error.message);
+  }
+});
+
+promise
+  .then((response) => console.log(response))
+  .catch(function (err) {
+    console.log(err);
+  });
