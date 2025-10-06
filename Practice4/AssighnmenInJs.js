5830ea6af645eadaaaf859437b36ea2f16dc85a2
mahfoozalamcse <amahfooz1007@gmail.com>
2025-10-07 04:34:18 +0530

    add practice module

---
+++ Practice4/AssighnmenInJs.js
@@ -0,0 +1,205 @@
+// Assignment 1
+// Assignment 1: Here is a list of items. Add a remove button for every item. With every click of the remove button, remove one item from the list.
+let products = [
+ {
+   id: "1",
+   item: "Shirt"
+ },
+ {
+   id: "2",
+   item: "Jeans"
+ },
+ {
+   id: "3",
+   item: "T-shirt"
+ },
+ {
+   id: "4",
+   item: "Denim Jacket"
+ },
+ {
+   id: "5",
+   item: "Casual Shoes"
+ }
+];
+
+// Assighnment 2
+// Assignment 2: Create an input field and take string input in it from the user. Render the input value on the viewport. Create a button and on click of the button reverse the input and display it on the view.
+
+
+
+// Assighnment 3
+// Assignment 3: Given the below data for clothing stock.
+const clothingStock = [
+ {
+   id: "1",
+   color: "red",
+   category: "men shirt",
+   item: "Regular Fit Men Shirt",
+   inStock: true
+ },
+ {
+   id: "2",
+   color: "green",
+   category: "women shirt",
+   item: "Slim Fit Women Shirt",
+   inStock: true
+ },
+ {
+   id: "3",
+   color: "blue",
+   category: "men shirt",
+   item: "Oversized Men Shirt",
+   inStock: false
+ },
+ {
+   id: "4",
+   color: "yellow",
+   category: "men tshirt",
+   item: "Regular Fit Men Tshirt",
+   inStock: false
+ },
+ 
+ 
+{
+   id: "5",
+   color: "magenta",
+   category: "women jeans",
+   item: "Ankle Lenght Women Jeans",
+   inStock: true
+ },
+ {
+   id: "6",
+   color: "yellow",
+   category: "men shirt",
+   item: "Casual Fit Men Shirt",
+   inStock: true
+ },
+ {
+   id: "7",
+   color: "green",
+   category: "men jeans",
+   item: "Carrot Fit Men Jeans",
+   inStock: true
+ },
+ {
+   id: "8",
+   color: "red",
+   category: "women shirt",
+   item: "Casual Fit Women Shirt",
+   inStock: false
+ }
+];
+//  3a - Console all the different colors available for women's shirts
+
+//  3b - Console all the different colors available for men's shirts which are currently in stock.
+
+//  3c - Console an array which only contains the elements which are in stock.
+
+// Assignment 4: Write a JavaScript program to add items to favorites from the drop down list. If you select any item that should be added to the list and be displayed on the ui, if you click on the added item again then it should not add the same item again.
+
+
+// Assignment 5: Write a JavaScript program to highlight the word with more than 5 characters with green background.
+
+// Assignment 6: Given a table with 3 rows and 3 columns Write a JavaScript program to add a row and every click on button.
+
+// Assignment 7: Write a javaScript function to protect email addresses. The part before ‘@’ should be clipped into half followed by 3 dots (...)
+// E.g: 
+// Input: mahfooz123@gmail.com
+// Output: alam...@gmail.com
+
+// Assignment 8: Given an input string S that contains multiple words, you need to remove all the spaces present in the input string. There can be multiple spaces present after any word’
+// E.g: 
+// Input: "     I am      a     Web    Developer   "
+// Output: IamaWebDeveloper
+
+
+// Assignment 9: Given an object obj, we have to invert its key and value for each entry. Inversion will result in value being used as key and key as value.
+
+// E.g:
+// Input:  let obj = { name: "Prakash", age: 111, hairColor: "black", likes: "food" };
+// Output: { '111': 'age', Prakash: 'name', black: 'hairColor', food: 'likes' }
+
+
+// Assignment 10: Given an array of numbers, we have to find the number with the most frequency and return it. If there are two or more numbers with the same most frequency then return the biggest number.
+
+// E.g: 
+// Input: const numbers = [1, 2, 3, 4, 5, 4, 3, 4, 3, 2, 1, 6, 7, 5];
+// Output: 4
+
+// Assignment 11: Write a Javascript program to create a Rating Component.
+
+// Assignment 12: Write a Prototype of subString Methods to find all the substring of a given string
+ 
+// Input: geeksforgeeks
+// Output: ['g', 'ge', 'gee', 'geek', 'geeks', 'e', 'ee', 'eek', 'eeks', 'e', 'ek', 'eks', 'k', 'ks', 's']
+
+// Assignment 13: Add cars brand, model and year to the list
+//Assignment 14: Sort Products By Price
+const initialData = [
+ {
+   id: 1,
+   item: "shirt",
+   price: 300
+ },
+ {
+   id: 2,
+   item: "tshirt",
+   price: 499
+ },
+ {
+   id: 3,
+   item: "jeans",
+   price: 500
+ },
+ {
+   id: 4,
+   item: "trousers",
+   price: 250
+ },
+ {
+   id: 5,
+   item: "chinos",
+   price: 300
+ }
+];
+
+// Assignment 15: Update Price of a Product by using it’s Id
+const initialData2 = [
+ {
+   id: 1,
+   item: "shirt",
+   price: 300
+ },
+ {
+   id: 2,
+   item: "tshirt",
+   price: 499
+ },
+ {
+   id: 3,
+   item: "jeans",
+   price: 500
+ },
+ {
+   id: 4,
+   item: "trousers",
+   price: 250
+ },
+ {
+   id: 5,
+   item: "chinos",
+   price: 300
+ }
+];
+
+// Assignment 16: Increase/Decrease Price of a Product
+
+
+ 
+ 
+
+
+
+
+
