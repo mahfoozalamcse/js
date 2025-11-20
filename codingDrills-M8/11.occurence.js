c41052a679ef7e1488b33c2ae17b268e2b007fd0
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-11-20 16:11:27 +0530

    Refactor occurrence counting using reduce method

--- codingDrills-M8/11.occurence.js
+++ codingDrills-M8/11.occurence.js
@@ -11,6 +11,7 @@
 const arr = ["a", "b", "c", "c", "d", "e", "b", "f", "a"];
 
 const result = arr.reduce(
+  
   (acc, cur) =>
     cur in acc ? { ...acc, [cur]: acc[cur] + 1 } : { ...acc, [cur]: 1 },
   {}
